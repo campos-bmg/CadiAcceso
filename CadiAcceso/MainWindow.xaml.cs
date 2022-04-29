999361ca1b17b098fad17b956e131c891815436e
campos-bmg <91045702+campos-bmg@users.noreply.github.com>
2022-04-29 17:22:00 -0500

    c12

--- CadiAcceso/MainWindow.xaml.cs
+++ CadiAcceso/MainWindow.xaml.cs
@@ -4,7 +4,7 @@ using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
-
+using MySql.Data;
 namespace CadiAcceso
 {
     /// <summary>
